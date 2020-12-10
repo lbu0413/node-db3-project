db80289e274f71358a7474cb3d72e0eb2f35f228
LBU <lbu@LBUui-MacBook-Pro.local>
2020-12-09 19:39:30 -0500

    second commit, working on scheme model

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,27 @@
 // scheme-model
+const db = require('../../data/db-config')
+
+module.exports = {
+    find() {
+        return db('schemes')
+    },
+    findById(id) {
+        if(!id){
+            return Promise.resolve(null)
+        }
+        else{
+            return db('schemes').where({ id }).first()
+        }
+    },
+    findSteps(id) {
+        return db('schemes')
+        .join('steps', 'steps.scheme_id', 'schemes.id')
+        .select('schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+        .where('schemes.id', id)
+    },
+    add(scheme) {
+        return db('schemes').insert(scheme).value('schemes.scheme_name')
+    }
+
+
+}
